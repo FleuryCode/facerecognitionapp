c31fa93b8376d2f36aa977060954d53a3959df23
Fleury Code <julienfleury93@gmail.com>
2022-01-22 18:46:49 +0100

    Adding the FaceRecogniton box and displaying it with CSS

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import Rank from './components/Rank/Rank';
 import Clarifai from 'clarifai';
-import {apiKeysData} from './components/ApiKeys/ApiKeys.js';
+import { apiKeysData } from './components/ApiKeys/ApiKeys.js';
 
 
 
@@ -21,27 +21,43 @@ class App extends React.Component {
     this.state = {
       input: '',
       imageUrl: '',
+      box: {},
     }
   }
 
-  
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage')
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol : width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+    
+  }
+
+
   onInputChange = (event) => {
-    this.setState({input: event.target.value})
+    this.setState({ input: event.target.value })
   }
 
   onSubmit = () => {
-    this.setState({imageUrl: this.state.input});
-    app.models.predict(
+    this.setState({ imageUrl: this.state.input });
+    app.models
+    .predict(
       Clarifai.FACE_DETECT_MODEL,
       this.state.input)
-      .then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function(err) {
-
-      }
-    );
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
   }
 
   render() {
@@ -51,14 +67,11 @@ class App extends React.Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit} />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
-        {/* {
-        
-        <FaceRecognition />} */}
+        <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box}/>
       </div>
     );
   }
-  }
-  
+}
+
 
 export default App;
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,12 +4,15 @@ import './Logo.css';
 
 const Logo = () => {
     return (
-        <a className="logoTag" href="https://j-squaredstudios.com" target={"_blank"} rel={"noreferrer"}>
-            <div className="logoContainer">
-            <img style={{height: 150, width: 150}} src={logoFile} alt="J2 Logo" />
+        <div className="mainLogoContainer">
+            <a className="logoTag" href="https://j-squaredstudios.com" target={"_blank"} rel={"noreferrer"}>
+                <div className="logoContainer">
+                    <img style={{ height: 150, width: 150 }} src={logoFile} alt="J2 Logo" />
+                </div>
+            </a>
         </div>
-        </a>
-        
+
+
     );
 }
 
